c32fb8250879b735bbbb7ab7265a033fccf05ab5
Jeromil Rodil <jeromilrodil1@gmail.com>
2019-10-17 12:30:27 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var _CONTENT = ["Front-End Developer...", "Back-End Developer...", "Html Css Bootstrap Js Jquery...", "Just kidding Haha..."];
+var _PART = 0;
+var _PART_INDEX = 0;
+var _INTERVAL_VAL;
+var _ELEMENT = document.querySelector("#text");
+function Type() { 
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+	if(text === _CONTENT[_PART]) {
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 20);
+		}, 1000);
+	}
+}
+function Delete() {
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+		if(_PART == (_CONTENT.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+		_PART_INDEX = 0;
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Type, 50);
+		}, 50);
+	}
+}
+_INTERVAL_VAL = setInterval(Type, 50);
